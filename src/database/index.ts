956b0cc4a05cc90a6605dc509f8adc7f8817f8ee
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-16 16:02:46 -0300

    feat: added initial server configs

---
+++ src/database/index.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+
+mongoose.connect();
---
+++ src/server/Server.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const Server = express();
+
+
+export { Server }
