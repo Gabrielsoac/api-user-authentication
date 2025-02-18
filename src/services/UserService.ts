0b9078ddb8de21bc22e8f1ee39c0d1538b843d5f
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-18 11:08:26 -0300

    feat: added get all users method

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,3 +1,5 @@
+import { TCreateUserRequestDto } from "../controllers/dtos/TCreateUserRequestDto";
+import { TGetUserRequestDto } from "../controllers/dtos/TGetUserRequestDto";
 import { IUserRepository } from "../repositories/IUserRepository";
 import { IUserService } from "./IUserService";
 import { TUserPersisted } from "./TUserPersisted";
@@ -25,5 +27,26 @@ export class UserService implements IUserService {
         } catch(err){
             throw new Error((err as Error).message);
         }
+    }
+
+    async getUsers(): Promise<TUserPersisted[]> {
+        try {
+            return await this.userRepository.findUsers();
+        }
+        catch(err){
+            throw new Error((err as Error).message);
+        }
+    }
+
+    async createUser(userData: TCreateUserRequestDto): Promise<TUserPersisted> {
+        throw new Error("Method not implemented.");
+    }
+
+    async updateUser(userId: TGetUserRequestDto, userData: TCreateUserRequestDto): Promise<TUserPersisted> {
+        throw new Error("Method not implemented.");
+    }
+
+    async deleteUser(userId: TGetUserRequestDto): Promise<void> {
+        throw new Error("Method not implemented.");
     }
 }
