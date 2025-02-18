36aca3ec6c6f787b5e587bdd96e96f5062e23ff9
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-18 09:42:35 -0300

    feat: created crud methods for user entity

--- src/controllers/IUserController.ts
+++ src/controllers/IUserController.ts
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { Request, Response } from "express";
+import { TUserResponseDto } from "./dtos/TUserResponseDto";
+import { TGetUserRequestDto } from "./dtos/TGetUserRequestDto";
+import { TCreateUserRequestDto } from "./dtos/TCreateUserRequestDto";
 
 export interface IUserController {
-    getUser(req: Request, res: Response): Promise<unknown>
+    getUser(req: Request<TGetUserRequestDto, {}, {}>, res: Response<TUserResponseDto>): void
+    listUsers(_: Request, Response: TUserResponseDto[]): void
+    registerUser(req: Request<{}, {}, TCreateUserRequestDto>, res: Response<TUserResponseDto>): void
+    updateUser(req: Request<TGetUserRequestDto, {}, TCreateUserRequestDto>, res: Response<TUserResponseDto>): void
+    deleteUser(req: Request<TGetUserRequestDto>, res: Response<void>): void
 }
