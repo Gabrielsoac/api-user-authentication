4fe74d82e025977c6213dee114878749b3ff9545
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-18 09:51:35 -0300

    feat: created users repository methods interface

--- src/repositories/IUserRepository.ts
+++ src/repositories/IUserRepository.ts
@@ -1,5 +1,11 @@
+import { TCreateUserRequestDto } from "../controllers/dtos/TCreateUserRequestDto";
+import { TGetUserRequestDto } from "../controllers/dtos/TGetUserRequestDto";
 import { TUserPersisted } from "../services/TUserPersisted";
 
 export interface IUserRepository {
     findUserById(userID: string): Promise<TUserPersisted>;
+    findUsers(): Promise<TUserPersisted[]>;
+    createUser(userData: TCreateUserRequestDto): Promise<TUserPersisted>;
+    updateUserById(userId: TGetUserRequestDto, userData: TCreateUserRequestDto): Promise<TUserPersisted>
+    deleteUserById(userId: TGetUserRequestDto): Promise<void>
 }
