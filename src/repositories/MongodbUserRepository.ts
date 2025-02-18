681a8b118f2773aeae81e1f3f87dac07f6558115
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-18 10:13:46 -0300

    feat: added get all users and create user

--- src/repositories/MongodbUserRepository.ts
+++ src/repositories/MongodbUserRepository.ts
@@ -1,26 +1,73 @@
 // import { UserModel } from "../database/model/UserSchema";
+import { TCreateUserRequestDto } from "../controllers/dtos/TCreateUserRequestDto";
+import { TGetUserRequestDto } from "../controllers/dtos/TGetUserRequestDto";
+import { UserModel } from "../database/model/UserSchema";
 import { TUserPersisted } from "../services/TUserPersisted";
 import { IUserRepository } from "./IUserRepository";
 
 export class MongoDbUserRepository implements IUserRepository {
     
+    async findUsers(): Promise<TUserPersisted[]> {
+        try {
+            const users = await UserModel.find();
+
+            const usersPersisted = users.map(
+                (x) => {
+                    return {
+                        id: x._id.toString(),
+                        username: x.username,
+                        email: x.email,
+                    }
+                }
+            );
+
+            return usersPersisted;
+        }
+        catch(err){
+            throw new Error((err as Error).message);
+        }
+    }
+    
+    async createUser(userData: TCreateUserRequestDto): Promise<TUserPersisted> {
+        try {
+            const user = await UserModel.create(
+                {...userData}
+            );
+
+            return {
+                id: user._id.toString(),
+                username: user.username,
+                email: user.email
+            }
+        }
+        catch(err){
+            throw new Error((err as Error).message);
+        }
+    }
+    
+    updateUserById(userId: TGetUserRequestDto, userData: TCreateUserRequestDto): Promise<TUserPersisted> {
+        throw new Error("Method not implemented.");
+    }
+    
+    deleteUserById(userId: TGetUserRequestDto): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    
     async findUserById(userID: string): Promise<TUserPersisted> {
         
         try {
-            // const user = await UserModel.findById(userID);
+            const user = await UserModel.findById(userID);
 
-            // if(!user){
-            //     throw new Error('Erro ao buscar usuário');
-            // }
-
-            console.log('cheguei no mongodb user repository, id:' + userID);
-
-            return {
-                id: "Teste ID",
-                username: "Teste username",
-                email: "user.email"
+            if(!user){
+                throw new Error('Erro ao buscar usuário');
             }
-        } catch(err){
+            return {
+                id: user._id.toString(),
+                username: user.username,
+                email: user.email
+            }
+        } 
+        catch(err){
             throw new Error((err as Error).message);
         }
     }
