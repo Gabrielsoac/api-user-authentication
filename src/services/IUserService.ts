6a68d3954a10646e4d4af2badc3478eff45ff8cc
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-18 11:07:23 -0300

    feat: added User Service methods Interface

--- src/services/IUserService.ts
+++ src/services/IUserService.ts
@@ -1,6 +1,12 @@
+import { TCreateUserRequestDto } from "../controllers/dtos/TCreateUserRequestDto";
+import { TGetUserRequestDto } from "../controllers/dtos/TGetUserRequestDto";
 import { TUserPersisted } from "./TUserPersisted";
 
 export interface IUserService {
 
     getUser(id: string): Promise<TUserPersisted>;
+    getUsers(): Promise<TUserPersisted[]>;
+    createUser(userData: TCreateUserRequestDto): Promise<TUserPersisted>;
+    updateUser(userId: TGetUserRequestDto, userData: TCreateUserRequestDto): Promise<TUserPersisted>
+    deleteUser(userId: TGetUserRequestDto): Promise<void>
 }
