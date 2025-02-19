8af5dcf4fa7395d2e9a6052e9bf048c3c5abfd04
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-19 07:37:35 -0300

    feat: get users controller method

--- src/controllers/IUserController.ts
+++ src/controllers/IUserController.ts
@@ -6,7 +6,7 @@ import { TCreateUserRequestDto } from "./dtos/TCreateUserRequestDto";
 
 export interface IUserController {
     getUser(req: Request<TGetUserRequestDto, {}, {}>, res: Response<TUserResponseDto>): void
-    listUsers(_: Request, Response: TUserResponseDto[]): void
+    listUsers(_: Request, res: Response<TUserResponseDto[]>): void
     registerUser(req: Request<{}, {}, TCreateUserRequestDto>, res: Response<TUserResponseDto>): void
     updateUser(req: Request<TGetUserRequestDto, {}, TCreateUserRequestDto>, res: Response<TUserResponseDto>): void
     deleteUser(req: Request<TGetUserRequestDto>, res: Response<void>): void
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { Request, Response } from "express";
 import { IUserController } from "./IUserController";
 import { StatusCodes } from "http-status-codes";
 import { IUserService } from "../services/IUserService";
+import { TGetUserRequestDto } from "./dtos/TGetUserRequestDto";
+import { TUserResponseDto } from "./dtos/TUserResponseDto";
+import { TCreateUserRequestDto } from "./dtos/TCreateUserRequestDto";
 
 export class UserController implements IUserController {
 
@@ -9,14 +13,29 @@ export class UserController implements IUserController {
         this.userService = userService;
     }
 
-    async getUser(req: Request, res: Response) {
+    async listUsers(_: Request, res: Response<TUserResponseDto[]>) {
+        try {
+            const users = await this.userService.getUsers();
+            res.status(StatusCodes.OK).json(users);
+        }
+        catch(err){
+            throw new Error((err as Error).message);
+        }
+    }
+    registerUser(req: Request<{}, {}, TCreateUserRequestDto>, res: Response<TUserResponseDto>): void {
+        throw new Error("Method not implemented.");
+    }
+    updateUser(req: Request<TGetUserRequestDto, {}, TCreateUserRequestDto>, res: Response<TUserResponseDto>): void {
+        throw new Error("Method not implemented.");
+    }
+    deleteUser(req: Request<TGetUserRequestDto>, res: Response<void>): void {
+        throw new Error("Method not implemented.");
+    }
+
+    async getUser(req: Request<TGetUserRequestDto>, res: Response<TUserResponseDto | unknown>) {
 
         try {
-            console.log('Cheguei no Controller');
-            console.log(req.params.id);
-            console.log(this.userService);
             const user = await this.userService.getUser(req.params.id);
-            console.log('passei do controller');
             res.status(StatusCodes.OK).json(user);
         } catch(err) {
             res.status(StatusCodes.NOT_FOUND).json(
